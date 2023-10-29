5077f8d0847b2a4b53e50b51eed2c1b4ea3bb9a7
artichokeee <rusau.qalife@gmail.com>
2023-10-29 21:29:37 +0300

    added lesson_2_4_1.js

--- lesson_2_4_1.js
+++ lesson_2_4_1.js
@@ -16,7 +16,36 @@ let expensesExamples = [
   },
 ];
 
-//Скорее всего я изначально неправильно понял задание и сделал расчет для трех лет
+function calculateYearlyExpenses(index) {
+    const yearlyExpences = expensesExamples[index].yearlyExpences;
+    let sum = 0;
+  
+    for (let i = 0; i < yearlyExpences.length; i++) {
+      if (yearlyExpences[i] > 1000) {
+        sum += yearlyExpences[i];
+      }
+    }
+  
+    return sum;
+  }
+
+  function testCalculateYearlyExpenses() {
+    expensesExamples.forEach((expenseExample, index) => {
+        const calculatedSum = calculateYearlyExpenses(index);
+
+        const expectedSums = [40590, 148200, 25709];
+
+        if (calculatedSum === expectedSums[index]) {
+            console.log(`Тест для объекта ${index} пройден.`);
+        } else {
+            console.log(`Тест для объекта ${index} не пройден.`);
+        }
+    });
+}
+
+testCalculateYearlyExpenses();
+  
+//Изначально неправильно понял задание и сделал расчет для трех лет
 
 // let expensesMatrix = [];
 
@@ -36,25 +65,10 @@ let expensesExamples = [
 
 // console.log(expensesSum);
 
-//Вот для одного массива
-
-let yearlyExpences1 = [
-  2500, 6000, 7000, 6000, 3, 9, 700, 8000, 655, 5700, 340, 5390,
-];
-
-function expensesCalculation (array){
-    let sum = 0
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] > 1000) {
-          sum += array[i];
-        }
-      }
-}
 
 
 
 
-// console.log(expensesCalculation(expensesExamples));
 
 
 
